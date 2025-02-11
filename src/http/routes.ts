8b9f052298734c2e0313dd3cd09108a39f366932
Luis Fernando <luisfernando.mourag@gmail.com>
2025-02-11 12:56:33 -0300

    Controller de registro

---
+++ src/http/routes.ts
@@ -0,0 +1,9 @@
+import type { FastifyInstance } from 'fastify';
+import { register } from './controllers/register';
+
+export async function appRoutes(app: FastifyInstance) {
+    app.post('/users', register)
+}
+
+
+
