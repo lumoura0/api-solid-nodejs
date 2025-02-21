c69bbb2fe7344033cbd4a084f2f3a6a973dd7920
Luis Fernando <luisfernando.mourag@gmail.com>
2025-02-21 17:49:02 -0300

    Criando Caso de uso de métricas

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -29,7 +29,12 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
         return this.items
             .filter((item) => item.user_id === userId)
             .slice((page - 1) * 20, page * 20)
+    }
 
+    async countByUserId(userId: string) {
+        return this.items
+            .filter((item) => item.user_id === userId)
+            .length
     }
 
     async create(data: Prisma.CheckInUncheckedCreateInput) {
