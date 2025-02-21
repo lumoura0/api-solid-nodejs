c494c8f09658d97278d733397753bfc56ad6ce81
Luis Fernando <luisfernando.mourag@gmail.com>
2025-02-21 17:36:21 -0300

    Criando Caso de uso de histórico

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -1,5 +1,7 @@
-import { Prisma, type CheckIn } from '@prisma/client'
+import { Prisma, CheckIn } from '@prisma/client'
+
 export interface CheckInsRepository {
-    create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
     findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
+    findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+    create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
 }
