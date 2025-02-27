09a7401cc6a6f7c9905159d2d05668f6f8109293
Luis Fernando <luisfernando.mourag@gmail.com>
2025-02-26 21:12:10 -0300

    Criando Repositórios do Prisma

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,10 +1,15 @@
 import { prisma } from '@/lib/prisma'
-import { Prisma, type User } from '@prisma/client'
+import { Prisma } from '@prisma/client'
 import type { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
-    findById(id: string): Promise<User | null> {
-        throw new Error('Method not implemented.')
+    async findById(id: string) {
+        const user = await prisma.user.findUnique({
+            where: {
+                id,
+            },
+        })
+        return user
     }
     async findByEmail(email: string) {
         const user = await prisma.user.findUnique({
